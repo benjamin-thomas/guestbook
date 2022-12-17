8f4d66f190e2d90a5e2fc48071a990aeb72c2c62
Benjamin <benjamin.guy.thomas@gmail.com>
2022-12-17 19:56:17 +0100

    Add queries

--- src/clj/guestbook/db/core.clj
+++ src/clj/guestbook/db/core.clj
@@ -1,16 +1,20 @@
 (ns guestbook.db.core
   (:require
-    [next.jdbc.date-time]
-    [next.jdbc.result-set]
-    [conman.core :as conman]
-    [mount.core :refer [defstate]]
-    [guestbook.config :refer [env]]))
+   [next.jdbc.date-time]
+   [next.jdbc.result-set]
+   [conman.core :as conman]
+   [mount.core :refer [defstate]]
+   [guestbook.config :refer [env]]))
 
 (defstate ^:dynamic *db*
-          :start (conman/connect! {:jdbc-url (env :database-url)})
-          :stop (conman/disconnect! *db*))
+  :start (conman/connect! {:jdbc-url (env :database-url)})
+  :stop (conman/disconnect! *db*))
 
 (conman/bind-connection *db* "sql/queries.sql")
+
+;; (defn messages-asc
+;;   (->> (get-messages)
+;;        (sort-by :timestamp #(compare %1 %2))))
 
 (extend-protocol next.jdbc.result-set/ReadableColumn
   java.sql.Timestamp
